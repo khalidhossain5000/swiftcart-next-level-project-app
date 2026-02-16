4f54d4baa535042607d4986e4a84fd51aafd013b
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-02-16 15:31:24 +0600

    products data load is added

--- js/products.js
+++ js/products.js
@@ -1,14 +1,45 @@
 const categoryContainer = document.getElementById("category-container");
+//loader start
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  // document.getElementById('word-card-container').classList.add('hidden');
+};
+//hide loader
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  // document.getElementById('word-card-container').classList.remove('hidden');
+};
+
+const removeActiveClass = () => {
+  const activeBtns = document.getElementsByClassName("active");
+  for (const btn of activeBtns) {
+    btn.classList.remove("active");
+  }
+};
 
 const loadCategories = () => {
+  showLoader();
   fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
     .then((categories) => {
       categories.unshift("All");
       displayCategories(categories);
+      hideLoader();
     });
 };
 
+// load all products data here
+
+const loadProducts=()=>{
+    showLoader()
+    fetch('https://fakestoreapi.com/products')
+    .then((res)=>res.json())
+    .then((products)=>{
+        displayProducts(products)
+           hideLoader();
+    })
+
+}
 
 const displayCategories = (categories) => {
   // get the container
@@ -17,10 +48,22 @@ const displayCategories = (categories) => {
   for (const category of categories) {
     const div = document.createElement("div");
     div.innerHTML = `
-    <button class='border border-gray-200 px-4 py-2 rounded-full text-xl text-[#5a6372]'> ${category}</button>
+    <button class='border-2 border-gray-200 px-4 py-2 rounded-full text-xl text-[#5a6372]'> ${category}</button>
     `;
     categoryContainer.appendChild(div);
   }
 };
+
+// display products
+
+const displayProducts=(products)=>{
+
+}
+
+
+
+
+
+
 
 loadCategories();
